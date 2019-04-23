ec0f7c2c4f295146f4e99a5c1853cfbff56c69fb
edwinachan <edwina.chan@bbc.co.uk>
2019-04-23 20:55:57 +0100

    Create mexican-wave.js

---
+++ mexican-wave.js
@@ -0,0 +1,31 @@
+/* Mexican wave
+
+Task
+ 	In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up.
+Rules
+ 	1.  The input string will always be lower case but maybe empty.
+
+2.  If the character in the string is whitespace then pass over it as if it was an empty seat.
+Example
+wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+*/
+
+function wave(string){
+  var stringLower = string.toLowerCase();
+  var toAry = [];
+  var wave = [];
+  for (i = 0; i < stringLower.length; i++) {
+      if(stringLower[i] !== ' ') {
+        toAry = stringLower.split('');
+        console.log(toAry)
+        toAry[i] = toAry[i].toUpperCase();
+        wave.push(toAry.join(''));
+        console.log(wave)
+      
+    }
+  }
+  return wave;
+}
+
+wave('hello f');
