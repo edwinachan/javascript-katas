12f62862d975697e2f05697cddaf59e0f54c8736
edwinachan <edwina.chan@bbc.co.uk>
2019-05-02 15:34:07 +0100

    Update regex-password-validation.js

--- regex-password-validation.js
+++ regex-password-validation.js
@@ -58,3 +58,13 @@ function validate(password) {
     } else {return false}
 
   }
+
+
+//Much nicer solution:
+
+function validate(password) {
+  return  /^[A-Za-z0-9]{6,}$/.test(password) &&
+          /[A-Z]+/           .test(password) &&
+          /[a-z]+/           .test(password) &&
+          /[0-9]+/           .test(password) ;
+}
