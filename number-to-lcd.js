45b7773ba3fa2388ecab0e5cf263c0c74168810a
Edwina Chan <edwina.chan@bbc.co.uk>
2019-07-12 15:56:09 +0100

    Updated solution to remove duplication so there is now just a single for loop which loops through the user's input to give the result. Makes use of storing the arrays for each digit in an object.

--- number-to-lcd.js
+++ number-to-lcd.js
@@ -1,3 +1,16 @@
+var digits = {
+    1: ['  ' , '| ', '| '],
+    2: [' _  ', ' _| ',  '|_  '],
+    3:  ['_  ', '_| ', '_| '],
+    4: ['    ', '|_| ', '  | '],
+    5: [' _  ', '|_  ', ' _| '],
+    6: [' _  ', '|_  ', '|_| '],
+    7: ['_  ', ' | ', ' | '],
+    8: [' _  ', '|_| ', '|_| '],
+    9: [' _  ', '|_| ', ' _| ']
+
+}
+
 function lcd(input, w, h) {
     var s = Array.from(input.toString()).map(Number)
 
@@ -23,7 +36,6 @@ function lcd(input, w, h) {
             }
         })
     }
-
 
     function changeHeight(array) {
         var indexShift = 0
@@ -81,15 +93,13 @@ function lcd(input, w, h) {
         }
     }
 
-
     generateNumberOfRows()
 
     for (var i = 0; i < s.length; i++) {
-        if (s[i] === 1) {
-            var one = ['  ' , '| ', '| ']
 
             var arrayOfNums = []
-            convertNumsToArrays(one)
+                
+            convertNumsToArrays(digits[s[i]])
 
             changeWidth(arrayOfNums)
 
@@ -97,131 +107,8 @@ function lcd(input, w, h) {
 
             changeHeight(arrayOfNums)
 
-            generateResult(arrayOfNumsWithHeight)
-
-
-        }
-
-
-        if (s[i] === 2) {
-            var two = [' _  ', ' _| ',  '|_  ']
-            var arrayOfNums = []
-            convertNumsToArrays(two)
-
-            changeWidth(arrayOfNums)
-
-            var arrayOfNumsWithHeight = arrayOfNums.slice()
-
-            changeHeight(arrayOfNums)
-
-            generateResult(arrayOfNumsWithHeight)
-        }
-
-        if (s[i] === 3) {
-
-            var three = ['_  ', '_| ', '_| ']
-            var arrayOfNums = []
-            convertNumsToArrays(three)
-
-            changeWidth(arrayOfNums)
-
-            var arrayOfNumsWithHeight = arrayOfNums.slice()
-
-            changeHeight(arrayOfNums)
-
-            generateResult(arrayOfNumsWithHeight)
-
-        }
-
-        if ( s[i] === 4) {
-            var four = ['    ', '|_| ', '  | ']
-            var arrayOfNums = []
-            convertNumsToArrays(four)
-
-            changeWidth(arrayOfNums)
-            var arrayOfNumsWithHeight = arrayOfNums.slice()
-
-            changeHeight(arrayOfNums)
-
-            generateResult(arrayOfNumsWithHeight)
-
-        }
-
-        if (s[i] === 5) {
-            var five = [' _  ', '|_  ', ' _| ']
-            var arrayOfNums = []
-            convertNumsToArrays(five)
-
-            changeWidth(arrayOfNums)
-
-            var arrayOfNumsWithHeight = arrayOfNums.slice()
-
-            changeHeight(arrayOfNums)
-
-            generateResult(arrayOfNumsWithHeight)
-        }
-
-        if (s[i] === 6) {
-
-            var six = [' _  ', '|_  ', '|_| ']
-            var arrayOfNums = []
-            convertNumsToArrays(six)
-
-            changeWidth(arrayOfNums)
-
-            var arrayOfNumsWithHeight = arrayOfNums.slice()
-
-            changeHeight(arrayOfNums)
-
-            generateResult(arrayOfNumsWithHeight)
-        }
-
-        if (s[i] === 7) {
-
-            var seven = ['_  ', ' | ', ' | ']
-            var arrayOfNums = []
-            convertNumsToArrays(seven)
-
-            changeWidth(arrayOfNums)
-
-            var arrayOfNumsWithHeight = arrayOfNums.slice()
-
-            changeHeight(arrayOfNums)
-
-            generateResult(arrayOfNumsWithHeight)
-        }
-
-        if (s[i] === 8) {
-
-            var eight = [' _  ', '|_| ', '|_| ']
-            var arrayOfNums = []
-            convertNumsToArrays(eight)
-
-            changeWidth(arrayOfNums)
-
-            var arrayOfNumsWithHeight = arrayOfNums.slice()
-
-            changeHeight(arrayOfNums)
-
-            generateResult(arrayOfNumsWithHeight)
-        }
-
-        if (s[i] === 9) {
-            var nine = [' _  ', '|_| ', ' _| ']
-
-            var arrayOfNums = []
-            convertNumsToArrays(nine)
-
-            changeWidth(arrayOfNums)
-
-            var arrayOfNumsWithHeight = arrayOfNums.slice()
-
-            changeHeight(arrayOfNums)
-
-            generateResult(arrayOfNumsWithHeight)
-        }
-
-
+            generateResult(arrayOfNumsWithHeight)  
+            
     }
 
     var result = ''
@@ -231,7 +118,6 @@ function lcd(input, w, h) {
     }
 
     return result
-
 
 }
 
