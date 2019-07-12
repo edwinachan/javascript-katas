c7747de200fde8abee393b3ed3fe716fac13f2b7
Edwina Chan <edwina.chan@bbc.co.uk>
2019-07-12 10:51:03 +0100

    Used digit '2' as a test for the changeHeight function (which works for now). Though it works, the solution needs to be updated so that the resulting number of rows takes into account the height input from the user.

--- number-to-lcd.js
+++ number-to-lcd.js
@@ -1,5 +1,8 @@
-function lcd(input, w) {
+function lcd(input, w, h) {
     var s = Array.from(input.toString()).map(Number)
+    console.log(s)
+
+    var rows = []
 
     function changeWidth(array) {
         array.forEach(function(item) {
@@ -20,16 +23,52 @@ function lcd(input, w) {
         })
     }
 
+    function changeHeight(array) {
+
+        var indexShift = 0
+
+        array.forEach(function(item) {
+
+          var index = array.indexOf(item)
+
+          if (item.includes('|')) {
+            var duplicateRow = item.slice()
+
+            for (i = 0; i < duplicateRow.length; i++) {
+              if (duplicateRow[i] !== '|') {
+                duplicateRow[i] = ' '
+              }
+            }
+
+
+            if (h > 1) {
+                for (i = 0; i < h; i++){
+                    arrayOfNumsWithHeight.splice(index + indexShift, 0, duplicateRow)
+                }
+            }
+            
+
+            indexShift += h
+
+          }
+
+        })
+    }
+
     function convertNumsToArrays(array) {
       array.forEach(function(item) {
         arrayOfNums.push(Array.from(item))
       })
     }
 
+
     var row1 = ''
     var row2 = ''
     var row3 = ''
-    var arrayOfNums = []
+    var row4 = ''
+    var row5 = ''
+    var row6 = ''
+    var row7 = ''
 
     for (i = 0; i < s.length; i++) {
         if (s[i] === 1) {
@@ -50,17 +89,22 @@ function lcd(input, w) {
 
         if (s[i] === 2) {
             var two = [' _  ', ' _| ',  '|_  ']
-
             var arrayOfNums = []
             convertNumsToArrays(two)
 
             changeWidth(arrayOfNums)
 
-            row1 += arrayOfNums[0].join('')
-            row2 += arrayOfNums[1].join('')
-            row3 += arrayOfNums[2].join('')
+            var arrayOfNumsWithHeight = arrayOfNums.slice()
 
+            changeHeight(arrayOfNums)
 
+            row1 += arrayOfNumsWithHeight[0].join('')
+            row2 += arrayOfNumsWithHeight[1].join('')
+            row3 += arrayOfNumsWithHeight[2].join('')
+            row4 += arrayOfNumsWithHeight[3].join('')
+            row5 += arrayOfNumsWithHeight[4].join('')
+            row6 += arrayOfNumsWithHeight[5].join('')
+            row7 += arrayOfNumsWithHeight[6].join('')
 
         }
 
@@ -71,9 +115,15 @@ function lcd(input, w) {
 
             changeWidth(arrayOfNums)
 
-            row1 += arrayOfNums[0].join('')
-            row2 += arrayOfNums[1].join('')
-            row3 += arrayOfNums[2].join('')
+            var arrayOfNumsWithHeight = arrayOfNums.slice()
+
+            changeHeight(arrayOfNums)
+
+            row1 += arrayOfNumsWithHeight[0].join('')
+            row2 += arrayOfNumsWithHeight[1].join('')
+            row3 += arrayOfNumsWithHeight[2].join('')
+            row4 += arrayOfNumsWithHeight[3].join('')
+            row5 += arrayOfNumsWithHeight[4].join('')
 
         }
 
@@ -83,10 +133,16 @@ function lcd(input, w) {
             convertNumsToArrays(four)
 
             changeWidth(arrayOfNums)
+            var arrayOfNumsWithHeight = arrayOfNums.slice()
 
-            row1 += arrayOfNums[0].join('')
-            row2 += arrayOfNums[1].join('')
-            row3 += arrayOfNums[2].join('')
+            changeHeight(arrayOfNums)
+
+            row1 += arrayOfNumsWithHeight[0].join('')
+            row2 += arrayOfNumsWithHeight[1].join('')
+            row3 += arrayOfNumsWithHeight[2].join('')
+            row4 += arrayOfNumsWithHeight[3].join('')
+            row5 += arrayOfNumsWithHeight[4].join('')
+
         }
 
         if (s[i] === 5) {
@@ -156,7 +212,10 @@ function lcd(input, w) {
 
     }
 
-    return row1 + '\n' + row2 + '\n' + row3
+
+    return row1 + '\n' + row2 + '\n' + row3 + '\n' + row4 + '\n' + row5 + '\n' + row6 + '\n' + row7
 }
 
-console.log(lcd(345, 2))
+console.log(lcd(2, 2, 2))
+
+
