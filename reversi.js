c0de24cf42eb0bc5d5ac41f086919213e45928fe
Edwina Chan <edwina.chan@bbc.co.uk>
2019-07-14 14:04:45 +0100

    Added function canGoDiagonallyUpRight.

--- reversi.js
+++ reversi.js
@@ -99,10 +99,38 @@ function reversi(input) {
         }
     }
 
+    function canGoDiagonallyUpRight() {
+
+        for (var i = 0; i < input.length-1; i++) {
+            if (input[i].includes(playerGoing)) {
+                var rowOfPlayer = i
+
+                for (var j = 0; j < input[i].length; j++) {
+                    if (input[i][j] === playerGoing) {
+                        var positionOfPlayer = j
+
+                        for (var k = rowOfPlayer-1; k > -1; k--) {
+                            if (input[k][positionOfPlayer + 1] === opposingPlayer) {
+                                positionOfPlayer ++
+                                continue
+                            } else if (input[k + 1][positionOfPlayer] === opposingPlayer && input[k][positionOfPlayer + 1] === '.') {
+                                input[k][positionOfPlayer + 1] = '0'
+                                break
+                            } else {
+                                continue
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     canGoLeft()
     canGoRight()
     canGoUp()
     canGoDown()
+    canGoDiagonallyUpRight()
 
     return input
 }
@@ -122,14 +150,14 @@ var testCase1 = [
 var testCase2 = [
     ['.', '.', '.', '.', '.', '.', '.', '.', ],
     ['.', '.', '.', '.', '.', '.', '.', '.', ],
-    ['.', '.', '.', '.', '.', '.', '.', '.', ],
+    ['.', '.', '.', '.', 'W', '.', '.', '.', ],
     ['.', '.', 'B', 'B', 'B', '.', '.', '.', ],
-    ['.', '.', 'B', 'B', 'B', '.', '.', '.', ],
-    ['.', '.', 'B', 'W', 'B', 'B', '.', '.', ],
+    ['.', '.', 'B', 'B', 'B', '.', 'W', '.', ],
+    ['.', '.', 'B', 'W', 'B', 'B', 'B', '.', ],
     ['.', '.', '.', '.', 'W', 'W', '.', '.', ],
     ['.', '.', '.', '.', '.', '.', '.', '.', ],
     ['B']
 ]
 
 
-console.log(reversi(testCase1))
+console.log(reversi(testCase2))
