dd067fd8cb36f6feebd338018694e48741ccd97e
Edwina Chan <edwina.chan@bbc.co.uk>
2019-07-16 14:40:06 +0100

    Solution has been shortened to contain only one function that is called eight times, instead of eight functions that are called once each (so eight calls in total).
    By using coordinates to indicate which direction we want to check (these are passed in as arguments), we are able to apply the same logic for each check, and therefore one function.

--- reversi.js
+++ reversi.js
@@ -9,97 +9,8 @@ function reversi(input) {
         opposingPlayer = 'B'
     }
 
-    function canGoLeft() {
 
-        for (var i = 0; i < input.length-1; i++) {
-            if (input[i].includes(playerGoing)) {
-                var columnOfPlayer = input[i].indexOf(playerGoing)
-
-                for  (var j = columnOfPlayer - 1; j > -1; j--) {
-                    if (input[i][j] === opposingPlayer) {
-                        continue
-                    } else if (input[i][j+1] === opposingPlayer && input[i][j] === '.') {
-                        input[i][j] = '0'
-                        break
-                    } else {
-                        break
-                    }
-                }
-            }
-        }
-    }
-
-    function canGoRight() {
-
-        for (var i = 0; i < input.length-1; i++) {
-            if (input[i].includes(playerGoing)) {
-                var columnOfPlayer = input[i].indexOf(playerGoing)
-
-                for (var j = columnOfPlayer + 1; j < 9; j++) {
-                    if (input[i][j] === opposingPlayer) {
-                        continue
-                    } else if (input[i][j-1] === opposingPlayer && input[i][j] === '.') {
-                        input[i][j] = '0'
-                        break
-                    } else {
-                        break
-                    }
-                }
-            }
-        }
-    }
-
-    function canGoUp() {
-        for (var i = 0; i < input.length-1; i++) {
-            if (input[i].includes(playerGoing)) {
-                var rowOfPlayer = i
-
-                for (var j = 0; j < input[rowOfPlayer].length; j++) {
-                    if (input[rowOfPlayer][j] === playerGoing) {
-                        var columnOfPlayer = j
-                        
-                        for (var k = rowOfPlayer - 1; k > -1; k--) {
-                            if (input[k][columnOfPlayer] === opposingPlayer) {
-                                continue
-                            } else if (input[k+1][columnOfPlayer] === opposingPlayer && input[k][columnOfPlayer] === '.') {
-                                input[k][columnOfPlayer] = '0'
-                            } else {
-                                break
-                            }
-                        }
-
-                    }
-                }
-            }
-
-        }
-    }
-
-    function canGoDown() {
-        for (var i = 0; i < input.length-1; i++) {
-            if (input[i].includes(playerGoing)) {
-                var rowOfPlayer = i
-
-                for (var j = 0; j < input[rowOfPlayer].length; j++ ) {
-                    if (input[rowOfPlayer][j] === playerGoing) {
-                        var columnOfPlayer = j
-
-                        for (var k = rowOfPlayer + 1; k < 9; k++ ) {
-                            if (input[k][columnOfPlayer] === opposingPlayer) {
-                                continue
-                            } else if(input[k-1][columnOfPlayer] === opposingPlayer && input[k][columnOfPlayer] === '.') {
-                                input[k][columnOfPlayer] = '0'
-                            } else {
-                                break
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    function canGoDiagonallyUpRight() {
+    function canGoInDirection({x, y}) {
 
         for (var i = 0; i < input.length-1; i++) {
             if (input[i].includes(playerGoing)) {
@@ -109,12 +20,12 @@ function reversi(input) {
                     if (input[rowOfPlayer][j] === playerGoing) {
                         var columnOfPlayer = j
 
-                        for (var k = rowOfPlayer-1; k > -1; k--) {
-                            if (input[k][columnOfPlayer + 1] === opposingPlayer) {
-                                columnOfPlayer ++
+                        for (var k = rowOfPlayer + y; k >= 0 && k < 9; k += y) {
+                            if (input[k][columnOfPlayer + x] === opposingPlayer) {
+                                columnOfPlayer +=x
                                 continue
-                            } else if (input[k + 1][columnOfPlayer] === opposingPlayer && input[k][columnOfPlayer + 1] === '.') {
-                                input[k][columnOfPlayer + 1] = '0'
+                            } else if (input[k - y][columnOfPlayer] === opposingPlayer && input[k][columnOfPlayer + x] === '.') {
+                                input[k][columnOfPlayer + x] = '0'
                                 break
                             } else {
                                 break
@@ -126,95 +37,21 @@ function reversi(input) {
         }
     }
 
-    function canGoDiagonallyUpLeft() {
-        for (var i = 0; i < input.length - 1; i++) {
-            if (input[i].includes(playerGoing)) {
-                var rowOfPlayer = i
 
-                for (var j = 0; j < input[rowOfPlayer].length; j++) {
-                    if (input[rowOfPlayer][j] === playerGoing) {
-                        var columnOfPlayer = j
+    canGoInDirection({x:0, y:1})
+    canGoInDirection({x:0, y:-1})
+    canGoInDirection({x:1, y:1})
+    canGoInDirection({x:-1, y:1})
+    canGoInDirection({x:1, y:-1})
+    canGoInDirection({x:-1, y:-1})
+    canGoInDirection({x:-1, y:0})
+    canGoInDirection({x:1, y:0})
 
-                        for (var k = rowOfPlayer-1; k > -1; k--) {
-                            if (input[k][columnOfPlayer-1] === opposingPlayer) {
-                                columnOfPlayer --
-                                continue
-                            } else if (input[k+1][columnOfPlayer] === opposingPlayer && input[k][columnOfPlayer - 1] === '.') {
-                                input[k][columnOfPlayer - 1] = '0'
-                                break
-                            } else {
-                                break
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    function canGoDiagonallyDownRight() {
-        for (var i = 0; i < input.length - 1; i++) {
-            if (input[i].includes(playerGoing)) {
-                var rowOfPlayer = i
-
-                for (var j = 0; j < input[rowOfPlayer].length; j++) {
-                    if (input[rowOfPlayer][j] === playerGoing) {
-                        var columnOfPlayer = j
-
-                        for (var k = rowOfPlayer + 1; k < 9; k++) {
-                            if (input[k][columnOfPlayer + 1] === opposingPlayer) {
-                                columnOfPlayer++
-                                continue
-                            } else if (input[k-1][columnOfPlayer] === opposingPlayer && input[k][columnOfPlayer + 1] === '.') {
-                                input[k][columnOfPlayer + 1] = '0'
-                                break
-                            } else {
-                                break
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    function canGoDiagonallyDownLeft () {
-        for (var i = 0; i < input.length - 1; i++) {
-            if (input[i].includes(playerGoing)) {
-                var rowOfPlayer = i
-
-                for (var j = 0; j < input[rowOfPlayer].length; j++) {
-                    if (input[rowOfPlayer][j] === playerGoing) {
-                        var columnOfPlayer = j
-
-                        for (var k = rowOfPlayer + 1; k < 9; k++) {
-                            if (input[k][columnOfPlayer - 1] === opposingPlayer) {
-                                columnOfPlayer--
-                                continue
-                            } else if (input[k-1][columnOfPlayer] === opposingPlayer && input[k][columnOfPlayer - 1] === '.') {
-                                input[k][columnOfPlayer - 1] = '0'
-                                break
-                            } else {
-                                break
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    canGoLeft()
-    canGoRight()
-    canGoUp()
-    canGoDown()
-    canGoDiagonallyUpRight()
-    canGoDiagonallyUpLeft()
-    canGoDiagonallyDownRight()
-    canGoDiagonallyDownLeft()
 
     return input
+
 }
+
 
 var testCase1 = [
     ['.', '.', '.', '.', '.', '.', '.', '.', ],
@@ -240,5 +77,17 @@ var testCase2 = [
     ['B']
 ]
 
+var testCase3 = [
+    ['.', '.', '.', '.', '.', '.', '.', '.', ],
+    ['.', '.', '.', '.', '.', '.', '.', '.', ],
+    ['.', '.', '.', 'B', 'B', 'B', '.', '.', ],
+    ['.', '.', '.', 'B', 'W', 'W', '.', '.', ],
+    ['.', '.', '.', 'B', 'W', '.', '.', '.', ],
+    ['.', '.', '.', '.', '.', '.', '.', '.', ],
+    ['.', '.', '.', '.', '.', '.', '.', '.', ],
+    ['.', '.', '.', '.', '.', '.', '.', '.', ],
+    ['B']
+]
 
-console.log(reversi(testCase2))
+
+console.log(reversi(testCase3))
